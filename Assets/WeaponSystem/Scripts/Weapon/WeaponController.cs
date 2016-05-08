33a1bad992b1816b0bf97862fdc75b02af9beeac
hopeliu <howui2014@foxmail.com>
2016-05-08 10:23:29 +0800

    init

---
+++ Assets/WeaponSystem/Scripts/Weapon/WeaponController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WeaponController : MonoBehaviour
+{
+    public GameObject[] WeaponLists;
+    public int CurrentWeapon = 0;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            LaunchWeapon();
+        }
+        else if (Input.GetButtonUp("Switch"))
+        {
+            SwitchWeapon();
+        }
+        else if (Input.GetButton("FireGun"))
+        {
+            LaunchWeapon(0);
+        }
+        else if (Input.GetButtonUp("FireLeft"))
+        {
+            LaunchWeapon(1);
+        }
+        else if (Input.GetButtonUp("FireRight"))
+        {
+            LaunchWeapon(2);
+        }
+        else if (Input.GetButtonUp("Cancel"))
+        {
+            //SceneManager.LoadScene(SceneManager.GetSceneAt(0).path);
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+
+        //float h = Input.GetAxis("Horizontal");
+        //float v = Input.GetAxis("Vertical");
+        //if (h >= float.Epsilon || h <= -float.Epsilon){
+        //    Debug.Log("X: " + h.ToString());
+        //}
+        //if (v >= float.Epsilon || v <= -float.Epsilon)
+        //{
+        //    Debug.Log("Y: " + v.ToString());
+        //}
+    }
+
+    public void SwitchWeapon (){
+        if (WeaponLists.Length > 0){
+            CurrentWeapon = (CurrentWeapon + 1) % WeaponLists.Length;
+        }       
+    }
+	
+	public void LaunchWeapon(int index){
+		CurrentWeapon = index;
+		if(CurrentWeapon < WeaponLists.Length && WeaponLists[index] != null){
+			WeaponLists[index].gameObject.GetComponent<WeaponLauncher>().Shoot();
+		}
+	}
+
+	public void LaunchWeapon(){
+		if(CurrentWeapon < WeaponLists.Length && WeaponLists[CurrentWeapon] != null){
+			WeaponLists[CurrentWeapon].gameObject.GetComponent<WeaponLauncher>().Shoot();
+		}
+	}
+}
