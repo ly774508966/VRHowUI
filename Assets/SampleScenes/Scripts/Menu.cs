7cffb727496942b187bdffc9640d6cb36636d165
hopeliu <maccw6513riod46@163.com>
2016-05-27 23:06:28 +0800

    add

---
+++ Assets/SampleScenes/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("joystick button 0")) {
+			Debug.Log ("SinglePlayer!!!");
+
+			SceneManager.LoadScene("AircraftJet2Axis");
+		}
+	}
+}
