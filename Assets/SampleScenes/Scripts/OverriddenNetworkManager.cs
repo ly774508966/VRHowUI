8fbdd364857996dcfa0df511dcb02772deed8ba1
hopeliu <howui2014@foxmail.com>
2016-05-31 16:16:51 +0800

    modify

--- Assets/SampleScenes/Scripts/OverriddenNetworkManager.cs
+++ Assets/SampleScenes/Scripts/OverriddenNetworkManager.cs
@@ -4,25 +4,25 @@ using System.Collections;
 
 public class OverriddenNetworkManager : NetworkManager {
 
-//	public override void OnServerAddPlayer (NetworkConnection conn, short playerControllerId)
-//	{
-//		GameObject player;
-//		//SpawnData spawnData = Tools.gameController.GetSpawnData();
-//
-//		player = (GameObject)Object.Instantiate(this.playerPrefab, Vector3.zero, Quaternion.identity);
-//		NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
-//
-//	}
-//
-//
-//
-//	public override void OnClientSceneChanged(NetworkConnection conn)
-//	{
-//		ClientScene.AddPlayer(conn, 0);
-//	}
-//
+	//public override void OnServerAddPlayer (NetworkConnection conn, short playerControllerId)
+	//{
+	//	GameObject player;
+	//	//SpawnData spawnData = Tools.gameController.GetSpawnData();
+
+	//	player = (GameObject)Object.Instantiate(this.playerPrefab, Vector3.zero, Quaternion.identity);
+	//	NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+
+	//}
+
+
+	public override void OnClientSceneChanged(NetworkConnection conn)
+	{
+		ClientScene.AddPlayer(conn, 0);
+	}
+
 	public override void OnClientConnect(NetworkConnection conn)
 	{
-		//ClientScene.Ready(conn);
+        Debug.Log("OnClientConnect " + conn.ToString());
+        //ClientScene.Ready(conn);
 	}
 }
