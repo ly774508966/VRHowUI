3c2b343211931184cf2b10b7de8d14e662bd921d
hopeliu <howui2014@foxmail.com>
2016-06-07 09:34:47 +0800

    modify

---
+++ Assets/SampleScenes/Scripts/Settings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Cancel")) {
+            SceneManager.LoadScene("Enter");
+        }
+    }
+}
